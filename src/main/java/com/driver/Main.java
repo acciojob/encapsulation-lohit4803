13d6e0cac3636d59625be44da63c08648ee9b0e7
lohit4803 <lohit4803@gmail.com>
2022-12-14 15:37:05 +0530

    encap

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
-public class Main {
-  
+public class Main
+{
+    public static void main(String[] args)
+    {
+        RWOnly q = new RWOnly();
+        q.setName("lenovo");
+        System.out.println(q.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class RWOnly {
+
+    String name;
+
+    public void setName(String s)
+    {
+        name = s;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
     
 }
